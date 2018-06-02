1ea69de6512d792a792f0cd5061631b533f71962
Jabooboo <39761338+Jabooboo@users.noreply.github.com>
2018-06-02 00:37:14 -0700

    Bugfix

--- legsandheads.py
+++ legsandheads.py
@@ -1,10 +1,9 @@
 '''task: there is 35 heads and 94 legs among roadrunners and coyotes, how many roadrunners and coyotes do we have'''
 
 def solver (heads,legs):
-    for roadrunner in range(heads):
-        coyote = 35 - roadrunner
+    for roadrunner in range(heads+1):
+        coyote = heads - roadrunner
         if(coyote*4 + roadrunner*2 == legs):
             print('Coyotes = ', coyote,' ', 'Roadrunners = ', roadrunner)
     return
-
 solver(35,94)
